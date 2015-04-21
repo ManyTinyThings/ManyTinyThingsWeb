e44b639d3c850dbad0d4e9b0abc149ab5d4ba313
Pontus Granström <pnutus@gmail.com>
2015-04-21 14:37:29 +0200

    Added adjustable box size

--- gl.js
+++ gl.js
@@ -10,75 +10,80 @@ var shaderProgram;
 
 function initGraphics(canvas) {
   
-  // init GL
-  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
-  if (!gl) {
-    console.log("Couldn't load WebGL.")
-  }
-  gl.viewportWidth = canvas.width;
-  gl.viewportHeight = canvas.height;
+    // init GL
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
+    if (!gl) {
+        console.log("Couldn't load WebGL.")
+    }
 
-  // vertex shader
+    // vertex shader
 
-  var vertexShader = loadShader(gl, gl.VERTEX_SHADER,[
-    "attribute vec2 vertexPosition;",
+    var vertexShader = loadShader(gl, gl.VERTEX_SHADER,[
+        "attribute vec2 vertexPosition;",
 
-    "uniform mat3 screenMatrix;",
-    "uniform mat3 viewMatrix;",
+        "uniform mat3 screenMatrix;",
+        "uniform mat3 viewMatrix;",
 
-    "void main(void) {",
-      "vec2 position = (screenMatrix * viewMatrix * vec3(vertexPosition, 1.0)).xy;",
-      "gl_Position = vec4(position, 0, 1);",
-    "}",
-  ].join("\n"));
+        "void main(void) {",
+        "vec2 position = (screenMatrix * viewMatrix * vec3(vertexPosition, 1.0)).xy;",
+        "gl_Position = vec4(position, 0, 1);",
+        "}",
+    ].join("\n"));
 
-  // fragment shader
+    // fragment shader
 
-  var fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, [
-    "precision mediump float;",
+    var fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, [
+        "precision mediump float;",
 
-    "void main(void) {",
-      "gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
-    "}",
-  ].join("\n"));
+        "void main(void) {",
+        "gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
+        "}",
+    ].join("\n"));
   
-  // set up shaders
+    // set up shaders
 
-  shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
 
-  gl.useProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
 
-  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertexPosition");
-  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
-  shaderProgram.viewMatrixUniform = gl.getUniformLocation(shaderProgram, "viewMatrix");
-  shaderProgram.screenMatrixUniform = gl.getUniformLocation(shaderProgram, "screenMatrix");
+    shaderProgram.viewMatrixUniform = gl.getUniformLocation(shaderProgram, "viewMatrix");
+    shaderProgram.screenMatrixUniform = gl.getUniformLocation(shaderProgram, "screenMatrix");
 
-  // set up screen
+    // set up screen
 
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
-  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 
-  // screen-space transform
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
+    mat3.identity(screenMatrix);
+    mat3.translate(screenMatrix, screenMatrix, [-1, -1]);
+    mat3.scale(screenMatrix, screenMatrix, [2 / gl.canvas.width, 2 / gl.canvas.height]);
+    gl.uniformMatrix3fv(shaderProgram.screenMatrixUniform, false, screenMatrix);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-  mat3.identity(screenMatrix);
-  mat3.translate(screenMatrix, screenMatrix, [-1, -1]);
-  mat3.scale(screenMatrix, screenMatrix, [2 / gl.viewportWidth, 2 / gl.viewportHeight]);
-  gl.uniformMatrix3fv(shaderProgram.screenMatrixUniform, false, screenMatrix);
+    // set up buffer
 
-  // set up buffer
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
+}
 
-  vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
-                         2, gl.FLOAT, false, 0, 0);
+function resize_canvas() {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
+    mat3.identity(screenMatrix);
+    mat3.translate(screenMatrix, screenMatrix, [-1, -1]);
+    mat3.scale(screenMatrix, screenMatrix, [2 / gl.canvas.width, 2 / gl.canvas.height]);
+    gl.uniformMatrix3fv(shaderProgram.screenMatrixUniform, false, screenMatrix);
 }
 
 function clear_canvas() {
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 function draw_ball(ball) {
@@ -87,24 +92,24 @@ function draw_ball(ball) {
 
 function drawDisc(center, radius, vertexCount) {
 
-  vertexCount = vertexCount || Math.max(Math.floor(2*radius), 10);
+    vertexCount = vertexCount || Math.max(Math.floor(2*radius), 10);
 
-  // generate vertices
+    // generate vertices
 
-  var vertices = new Float32Array(2 * vertexCount);
-  var angle = tau / vertexCount;
-  for (var i = 0; i < vertexCount; i++) {
-    vertices[2*i]     = Math.cos(i * angle);
-    vertices[2*i + 1] = Math.sin(i * angle);
-  }
+    var vertices = new Float32Array(2 * vertexCount);
+    var angle = tau / vertexCount;
+    for (var i = 0; i < vertexCount; i++) {
+        vertices[2*i]     = Math.cos(i * angle);
+        vertices[2*i + 1] = Math.sin(i * angle);
+    }
 
-  mat3.identity(viewMatrix);
-  mat3.translate(viewMatrix, viewMatrix, center);
-  mat3.scale(viewMatrix, viewMatrix, [radius, radius]);
-  gl.uniformMatrix3fv(shaderProgram.viewMatrixUniform, false, viewMatrix);
+    mat3.identity(viewMatrix);
+    mat3.translate(viewMatrix, viewMatrix, center);
+    mat3.scale(viewMatrix, viewMatrix, [radius, radius]);
+    gl.uniformMatrix3fv(shaderProgram.viewMatrixUniform, false, viewMatrix);
 
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  gl.drawArrays(gl.TRIANGLE_FAN, 0, vertexCount);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertexCount);
 }
 
 function loadShader(gl, type, str) {
