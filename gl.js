2c84d791871e40f8d2ad63ae08a0fb00823f49e5
Pontus Granström <pnutus@gmail.com>
2016-01-20 17:03:09 +0100

    Trajectories get their own shader

--- gl.js
+++ gl.js
@@ -6,49 +6,12 @@ var offsetBuffer;
 var colorBuffer;
 var scaleBuffer;
 var screenMatrix = mat3.create();
-var shaderProgram;
+var particleShaderProgram;
+var lineShaderProgram;
 
-function initGraphics(canvas) {
-
-    // init GL
-    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
-    if (!gl) {
-        console.log("Couldn't load WebGL.")
-    }
-
-    // vertex shader
-
-    var vertexShader = loadShader(gl, gl.VERTEX_SHADER,[
-        "attribute vec2 vertexPosition;",
-        "attribute vec2 offset;",
-        "attribute vec4 color;",
-        "attribute float scale;",
-
-        "uniform mat3 screenMatrix;",
-
-        "varying lowp vec4 vColor;",
-
-        "void main(void) {",
-            "vec2 newPosition = (vertexPosition * scale) + offset;",
-            "vec2 screenPosition = (screenMatrix * vec3(newPosition, 1.0)).xy;",
-            "gl_Position = vec4(screenPosition, 0, 1);",
-            "vColor = color;",
-        "}",
-    ].join("\n"));
-
-    // fragment shader
-
-    var fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, [
-        "varying lowp vec4 vColor;",
-
-        "void main(void) {",
-            "gl_FragColor = vColor;",
-        "}",
-    ].join("\n"));
-
-    // set up shaders
-
-    shaderProgram = gl.createProgram();
+function createShaderProgram(vertexShader, fragmentShader)
+{
+    var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
@@ -57,27 +20,91 @@ function initGraphics(canvas) {
       console.log("Could not initialise shaders");
       return null;
     }
+    
+    return shaderProgram;
+}
 
-    gl.useProgram(shaderProgram);
+function initGraphics(canvas) {
 
-    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertexPosition");
-    shaderProgram.offsetAttribute = gl.getAttribLocation(shaderProgram, "offset");
-    shaderProgram.colorAttribute  = gl.getAttribLocation(shaderProgram, "color");
-    shaderProgram.scaleAttribute  = gl.getAttribLocation(shaderProgram, "scale");
-
-    shaderProgram.screenMatrixUniform = gl.getUniformLocation(shaderProgram, "screenMatrix");
-
+    // init GL
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    if (!gl) {
+        console.log("Couldn't load WebGL.");
+        return;
+    }
+    
     // set up screen
-
-
+    
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     mat3.identity(screenMatrix);
     mat3.translate(screenMatrix, screenMatrix, [-1, -1]);
     mat3.scale(screenMatrix, screenMatrix, [2 / gl.canvas.width, 2 / gl.canvas.height]);
-    gl.uniformMatrix3fv(shaderProgram.screenMatrixUniform, false, screenMatrix);
+    
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    
+    // set up shaders
 
+    var lineVertexShader = loadShader(gl, gl.VERTEX_SHADER,[
+        "attribute vec2 vertexPosition;",
+        
+        "uniform vec4 color;",
+        "uniform mat3 screenMatrix;",
+    
+        "varying lowp vec4 vColor;",
+    
+        "void main(void) {",
+            "vec2 screenPosition = (screenMatrix * vec3(vertexPosition, 1.0)).xy;",
+            "gl_Position = vec4(screenPosition, 0, 1);",
+            "vColor = color;",
+        "}",
+    ].join("\n"));
+    
+    var particleVertexShader = loadShader(gl, gl.VERTEX_SHADER,[
+        "attribute vec2 vertexPosition;",
+        "attribute vec2 offset;",
+        "attribute vec4 color;",
+        "attribute float scale;",
+    
+        "uniform mat3 screenMatrix;",
+    
+        "varying lowp vec4 vColor;",
+    
+        "void main(void) {",
+            "vec2 newPosition = (vertexPosition * scale) + offset;",
+            "vec2 screenPosition = (screenMatrix * vec3(newPosition, 1.0)).xy;",
+            "gl_Position = vec4(screenPosition, 0, 1);",
+            "vColor = color;",
+        "}",
+    ].join("\n"));
+    
+    var fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, [
+        "varying lowp vec4 vColor;",
+    
+        "void main(void) {",
+            "gl_FragColor = vColor;",
+        "}",
+    ].join("\n"));
+    
+    particleShaderProgram = createShaderProgram(particleVertexShader, fragmentShader);
+    lineShaderProgram = createShaderProgram(lineVertexShader, fragmentShader);
+
+    gl.useProgram(particleShaderProgram);
+
+    particleShaderProgram.vertexPositionAttribute = gl.getAttribLocation(particleShaderProgram, "vertexPosition");
+    particleShaderProgram.offsetAttribute = gl.getAttribLocation(particleShaderProgram, "offset");
+    particleShaderProgram.colorAttribute  = gl.getAttribLocation(particleShaderProgram, "color");
+    particleShaderProgram.scaleAttribute  = gl.getAttribLocation(particleShaderProgram, "scale");
+    particleShaderProgram.screenMatrixUniform = gl.getUniformLocation(particleShaderProgram, "screenMatrix");
+    gl.uniformMatrix3fv(particleShaderProgram.screenMatrixUniform, false, screenMatrix);
+    
+    gl.useProgram(lineShaderProgram);
+    
+    lineShaderProgram.vertexPositionAttribute = gl.getAttribLocation(lineShaderProgram, "vertexPosition");
+    lineShaderProgram.screenMatrixUniform = gl.getUniformLocation(lineShaderProgram, "screenMatrix");
+    lineShaderProgram.colorUniform = gl.getUniformLocation(lineShaderProgram, "color");
+    gl.uniformMatrix3fv(lineShaderProgram.screenMatrixUniform, false, screenMatrix);
+    
     // set up buffers
 
     vertexBuffer = gl.createBuffer();
@@ -92,7 +119,6 @@ function resize_canvas() {
     mat3.identity(screenMatrix);
     var aspectRatio = gl.canvas.width / gl.canvas.height;
     mat3.scale(screenMatrix, screenMatrix, [1 / aspectRatio, 1]);
-    gl.uniformMatrix3fv(shaderProgram.screenMatrixUniform, false, screenMatrix);
 }
 
 function clear_canvas() {
@@ -126,44 +152,53 @@ function drawParticles(particles, radius) {
 
     // set attributes
 
+    gl.useProgram(particleShaderProgram);
+    
+    gl.uniformMatrix3fv(particleShaderProgram.screenMatrixUniform, false, screenMatrix);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(particleShaderProgram.vertexPositionAttribute);
+    gl.vertexAttribPointer(particleShaderProgram.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
 
     var ext = gl.getExtension("ANGLE_instanced_arrays");
 
     gl.bindBuffer(gl.ARRAY_BUFFER, offsetBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, offsets, gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(shaderProgram.offsetAttribute);
-    gl.vertexAttribPointer(shaderProgram.offsetAttribute, 2, gl.FLOAT, false, 0, 0);
-    ext.vertexAttribDivisorANGLE(shaderProgram.offsetAttribute, 1);
+    gl.enableVertexAttribArray(particleShaderProgram.offsetAttribute);
+    gl.vertexAttribPointer(particleShaderProgram.offsetAttribute, 2, gl.FLOAT, false, 0, 0);
+    ext.vertexAttribDivisorANGLE(particleShaderProgram.offsetAttribute, 1);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, scaleBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, radii, gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(shaderProgram.scaleAttribute);
-    gl.vertexAttribPointer(shaderProgram.scaleAttribute, 1, gl.FLOAT, false, 0, 0);
-    ext.vertexAttribDivisorANGLE(shaderProgram.scaleAttribute, 1);
+    gl.enableVertexAttribArray(particleShaderProgram.scaleAttribute);
+    gl.vertexAttribPointer(particleShaderProgram.scaleAttribute, 1, gl.FLOAT, false, 0, 0);
+    ext.vertexAttribDivisorANGLE(particleShaderProgram.scaleAttribute, 1);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(shaderProgram.colorAttribute);
-    gl.vertexAttribPointer(shaderProgram.colorAttribute, 4, gl.FLOAT, false, 0, 0);
-    ext.vertexAttribDivisorANGLE(shaderProgram.colorAttribute, 1);
+    gl.enableVertexAttribArray(particleShaderProgram.colorAttribute);
+    gl.vertexAttribPointer(particleShaderProgram.colorAttribute, 4, gl.FLOAT, false, 0, 0);
+    ext.vertexAttribDivisorANGLE(particleShaderProgram.colorAttribute, 1);
 
     ext.drawArraysInstancedANGLE(gl.TRIANGLE_FAN, 0, vertexCount, particleCount);
 }
 
-function drawTrajectory(trajectory)
+function drawTrajectory(trajectory, color)
 {
     var vertexCount = trajectory.length / 2;
     var vertices = new Float32Array(trajectory);
+    
+    gl.useProgram(lineShaderProgram);
+    
+    gl.uniformMatrix3fv(lineShaderProgram.screenMatrixUniform, false, screenMatrix);
+    
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
-
-    gl.disableVertexAttribArray(shaderProgram.offsetAttribute);
+    gl.enableVertexAttribArray(lineShaderProgram.vertexPositionAttribute);
+    gl.vertexAttribPointer(lineShaderProgram.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
+    
+    gl.uniform4fv(lineShaderProgram.colorUniform, color.rgba);
 
     gl.drawArrays(gl.LINE_STRIP, 0, vertexCount)
 }
