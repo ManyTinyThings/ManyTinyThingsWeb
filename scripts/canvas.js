2a3aee02f7d3aa9ed7dd9955a9d0d1bfab258c7d
Pontus Granström <pnutus@gmail.com>
2016-05-26 23:00:00 +0200

    draw arrows

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -64,51 +64,103 @@ function drawParticles(renderer, particles)
     }
 }
 
+function screenRelativeStroke(context)
+{
+    var c = context;
+    c.save();
+    c.setTransform(1, 0, 0, 1, 0, 0);
+    c.stroke();
+    c.restore();
+}
+
+function rotateToVector(context, v)
+{
+    context.transform(v[0], v[1], -v[1], v[0], 0, 0);
+}
+
+function drawArrow(renderer, start, end)
+{
+    // TODO: make this relative to pixels, not world coordinates
+    var maxArrowheadLength = 0.1;
+
+    var arrowVector = v2.alloc();
+    var shaftEnd = v2.alloc();
+
+    v2.subtract(arrowVector, end, start);
+    var arrowLength = v2.magnitude(arrowVector);
+
+    var arrowheadLength = atMost(maxArrowheadLength, arrowLength / 2);
+    var shaftLength = arrowLength - arrowheadLength;
+    v2.normalize(arrowVector, arrowVector);
+    v2.scaleAndAdd(shaftEnd, start, arrowVector, shaftLength);
+
+    var c = renderer.context;
+    c.beginPath();
+    c.moveTo(start[0], start[1]);
+    c.lineTo(shaftEnd[0], shaftEnd[1]);
+    screenRelativeStroke(c);
+
+
+    c.save();
+    // rotate and move to arrow shaft
+    c.translate(end[0], end[1]);
+    rotateToVector(c, arrowVector);
+
+    // draw arrowhead
+    c.beginPath();
+    c.moveTo(0, 0);
+    c.lineTo(-arrowheadLength, -arrowheadLength / 3);
+    c.lineTo(-arrowheadLength, arrowheadLength / 3);
+    c.closePath();
+    c.restore();
+
+    c.fillStyle = cssFromRGBA(colors.black.rgba);
+    c.fill();
+    c.restore();
+
+    v2.free(shaftEnd);
+    v2.free(arrowVector);
+}
+
 function drawTrajectoryUnzipped(renderer, xs, ys, color)
 {
-    var context = renderer.context;
-    context.strokeStyle = cssFromRGBA(color.rgba);
+    var c = renderer.context;
+    c.strokeStyle = cssFromRGBA(color.rgba);
 
-    context.beginPath();
-    context.moveTo(xs[0], ys[0]);
+    c.beginPath();
+    c.moveTo(xs[0], ys[0]);
     for (var i = 1; i < xs.length; i++)
     {
-        context.lineTo(xs[i], ys[i]);
+        c.lineTo(xs[i], ys[i]);
     }
-    context.save();
-    context.setTransform(1, 0, 0, 1, 0, 0);
-    context.stroke();
-    context.restore();
+    screenRelativeStroke(c);
 }
 
 function drawTrajectory(renderer, trajectory, color)
 {
-    var context = renderer.context;
-    context.strokeStyle = cssFromRGBA(color.rgba);
+    var c = renderer.context;
+    c.strokeStyle = cssFromRGBA(color.rgba);
     var startPoint = trajectory[0];
 
-    context.beginPath();
-    context.moveTo(startPoint[0], startPoint[1]);
+    c.beginPath();
+    c.moveTo(startPoint[0], startPoint[1]);
     for (var i = 1; i < trajectory.length; i++)
     {
         var point = trajectory[i];
-        context.lineTo(point[0], point[1]);
+        c.lineTo(point[0], point[1]);
     }
-    context.save();
-    context.setTransform(1, 0, 0, 1, 0, 0);
-    context.stroke();
-    context.restore();
+    screenRelativeStroke(c);
 }
 
 function drawRectangle(renderer, rectangle, color)
 {
-    var context = renderer.context;
-    context.fillStyle = cssFromRGBA(color.rgba);
+    var c = renderer.context;
+    c.fillStyle = cssFromRGBA(color.rgba);
     var topLeft = v2(rectangle.left, rectangle.top);
     var bottomRight = v2(rectangle.right, rectangle.bottom);
     var width = bottomRight[0] - topLeft[0];
     var height = bottomRight[1] - topLeft[1];
-    context.fillRect(topLeft[0], topLeft[1], width, height);
+    c.fillRect(topLeft[0], topLeft[1], width, height);
 }
 
 function clearRenderer(renderer)
