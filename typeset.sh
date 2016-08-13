9602408b3735faf1c2cfe8b94de7293ae207127c
Pontus Granström <pnutus@gmail.com>
2016-08-13 11:31:42 +0200

    attempt at using python to script

--- typeset.sh
+++ typeset.sh
@@ -1,7 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-for file in chapters/*.markdown; do
-	chapter_name=${file##*/}
-	chapter_name=${chapter_name%.markdown}
-	pandoc "$file" --template template.html --output "output/$chapter_name.html"
-done
+from glob import glob
+from subprocess import run
+import os
+import os.path as path
+
+for filename in glob("chapters/**/*.markdown", recursive=True):
+	output_filename = filename.replace("chapters/", "output/", 1)
+	dirname = path.dirname(output_filename)
+	os.makedirs(dirname, exist_ok=True)
+	if filename.endswith(".markdown"):
+		run(["pandoc", filename, "--template", "template.html", "--output", output_filename.replace(".markdown", ".html")])
+	else:
+		run(["cp", filename, output_filename])
